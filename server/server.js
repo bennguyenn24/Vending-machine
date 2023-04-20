d5b292e6164eba364d1c8eb23c18bc94147934a0
Ben Nguyen <bennguyenn24@gmail.com>
2023-04-20 13:20:45 -0700

    Added body parser

--- server/server.js
+++ server/server.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser")
 
 const corsOptions = {
     origin: "http://localhost:5173",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+// Middlewares
 app.use(cors(corsOptions));
+app.use(bodyParser.json())
 
 // Routes
 const itemRoutes = require("./routes/itemRoutes");
-
 app.use("/api/items", itemRoutes);
-
-// app.post("/api/snacks", async (req, res) => {
-//     // ROWS IS WHERE ALL INFO IS RETURNED
-//     const { rows } = await db.query("SELECT * FROM products");
-
-//     res.json({
-//         data: rows,
-//     });
-// });
 
 app.listen(3000, () => console.log("Server is running on PORT 3000"));

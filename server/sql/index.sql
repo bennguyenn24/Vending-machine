43252753d650912fe3a06914c165bbb974bb0167
Ben Nguyen <bennguyenn24@gmail.com>
2023-04-20 12:43:14 -0700

    Updated table with quantity

--- server/sql/index.sql
+++ server/sql/index.sql
@@ -5,9 +5,18 @@ CREATE DATABASE vending_machine;
 -- TODO: ADD A COLUMN FOR TYPES (SNACKS AND DRINKS)
 CREATE TABLE products(
    id serial PRIMARY KEY,
-   name VARCHAR (255) UNIQUE NOT NULL
+   name VARCHAR(255) UNIQUE NOT NULL,
+   price SMALLINT,
+   type VARCHAR(255)
 );
 
 -- INSERT INTO
 INSERT INTO products(name)
 VALUES ('Funyuns');
+
+-- DROP TABLE
+DROP TABLE products;
+
+-- Alter Table to Add Quantity Column 
+ALTER TABLE products
+ADD COLUMN quantity SMALLINT;
